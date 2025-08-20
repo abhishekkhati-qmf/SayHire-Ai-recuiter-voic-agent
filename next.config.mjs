64242c52fa7ee4ad6db2b9f3761089a043671bfd
abhishekkhati-qmf <abhishekkhati6235@gmail.com>
2025-08-20 21:31:25 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains:['img.freepik.com','lh3.googleusercontent.com','cdn-icons-png.flaticon.com','zvlbcadrwnbjbtulqvfl.supabase.co']
-    }
+  images: {
+    domains: [
+      'img.freepik.com',
+      'lh3.googleusercontent.com',
+      'cdn-icons-png.flaticon.com',
+      'zvlbcadrwnbjbtulqvfl.supabase.co'
+    ],
+  },
+  output: "standalone",   // 👈 important for Vercel builds
+  experimental: {
+    appDir: true,          // 👈 only if you are using /app router
+  },
 };
 
 export default nextConfig;
+
